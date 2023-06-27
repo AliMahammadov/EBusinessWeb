ae85ac640bb854550cd50ca895f4721d849aa752
AliMahammadov <e.mehemmedov99@gmail.com>
2023-06-28 01:45:35 +0400

    AddEmployeeCRUD

--- EBusinessWeb/Areas/Manage/Controllers/EmployeeController.cs
+++ EBusinessWeb/Areas/Manage/Controllers/EmployeeController.cs
@@ -1,14 +1,75 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EBusinessData.DAL;
+using EBusinessEntity.Entities;
+using EBusinessService.Services;
+using EBusinessViewModel.Entities.Employee;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace EBusinessWeb.Areas.Manage.Controllers
 {
     [Area("Manage")]
     public class EmployeeController : Controller
     {
+        private readonly IEmployeeService employeeService;
+        private readonly AppDbContext context;
 
-        public IActionResult Index()
+        public EmployeeController(IEmployeeService employeeService, AppDbContext context)
         {
+            this.employeeService = employeeService;
+            this.context = context;
+        }
+
+        public async Task<IActionResult>Index()
+        {
+            ICollection<Employee> employees = await employeeService.GetAllEmployee();
+
+            return View(employees);
+        }
+        public async Task<IActionResult> Create()
+        {
+            ViewBag.Position = new SelectList(context.Positions, nameof(Position.Id), nameof(Position.Name));
             return View();
         }
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateEmployeeVM employeeVM)
+        {
+            if(!ModelState.IsValid)
+            {
+                ViewBag.Positions = new SelectList(context.Positions, nameof(Position.Id),nameof(Position.Name));
+                return RedirectToAction(nameof(Create));
+            }
+            await employeeService.AddEmplooyeAsync(employeeVM);
+            return RedirectToAction(nameof(Create));
+        }
+        public async Task<IActionResult> Delete(int id)
+        {
+            if(id!=null )
+            {
+               await employeeService.RemoveEmployeeAsync(id);
+            }
+            return RedirectToAction($"{nameof(Index)}");
+        }
+        public async Task<IActionResult> Update(int id)
+        {
+            if(ModelState.IsValid)
+            {
+                ViewBag.Positions = new SelectList(context.Positions, nameof(Position.Id), nameof(Position.Name));
+            }
+            return RedirectToAction($"{nameof(Index)}");
+        }
+        [HttpPost]
+        public async Task<IActionResult> Update(int id, UpdateEmployeeVM employeeVM)
+        {
+            if (!ModelState.IsValid && employeeVM != null)
+            {
+                ViewBag.Positions = new SelectList(context.Positions, nameof(Position.Id), nameof(Position.Name));
+                return View();
+            }
+
+            await employeeService.UpdateEmployeeAsync(id, employeeVM);
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
+
